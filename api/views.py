f6b4cca11d1b46cc32a2ed7ce7a52346296cd9c1
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-09 17:00:11 +0500

    create customer

--- api/views.py
+++ api/views.py
@@ -1,16 +1,54 @@
 from django.views import View
 from django.http import HttpRequest, JsonResponse
+from .models import Customer, Contact
+from django.forms import model_to_dict
+import json
 
 
 class CustomersView(View):
     def get(self, request: HttpRequest) -> JsonResponse:
-        # TODO: get all customers
-        pass
+        """get all customers
+
+        Args:
+            request (HttpRequest): _description_
+
+        Returns:
+            JsonResponse: _description_
+        """
+        result = []
+        for customer in Customer.objects.all():
+            result.append(model_to_dict(customer, fields=['id', 'first_name', 'last_name', 'username']))
+
+        return JsonResponse(result, safe=False)
 
     def post(self, request: HttpRequest) -> JsonResponse:
-        # TODO: create a new customer
-        pass
+        """create a new customer
 
+        Args:
+            request (HttpRequest): _description_
+
+        Returns:
+            JsonResponse: _description_
+        """
+        data = json.loads(request.body.decode())
+
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        username = data.get('username')
+        password = data.get('password')
+
+        if all([first_name, last_name, username, password]):
+            Customer.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                password=password
+            )
+
+            return JsonResponse({'message': 'object created.'}, status=201)
+
+        else:
+            return JsonResponse({'error': 'invalid data.'}, status=404)
 
 class CustomerDetailsView(View):
     def get(self, request: HttpRequest, pk: int) -> JsonResponse:
