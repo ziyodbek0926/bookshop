b92ca03412e77ca31ae40349d67b9faeb53307f7
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-09 10:25:29 +0500

    contact

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('customers/', views.CustomerView.as_view()), # create, get all customers
     path('customers/<int:pk>/', views.CustomerDetailsView.as_view()), # get a customer, update, delete
-    path('contacts/', views.ContactView.as_view()),
-    path('contacts/<int:pk>/', views.ContactDetailsView.as_view()),
+    path('customers/<int:pk>/contact/', views.ContactView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.http import HttpRequest, JsonResponse
 from .models import Contact, Customer
 from django.forms import model_to_dict
+from django.core.exceptions import ObjectDoesNotExist
+import json
 
 
 class CustomerView(View):
@@ -28,19 +30,61 @@ class CustomerView(View):
 
         Returns:
             JsonResponse: _description_
-        """   
+        """ 
+        # TODO: create post view  
         pass     
 
 
-
 class CustomerDetailsView(View):
-    pass
+    def get(self, request: HttpRequest, pk: int) -> JsonResponse:
+        pass
+
+    def put(self, request: HttpRequest, pk: int) -> JsonResponse:
+        pass
+
+    def patch(self, request: HttpRequest, pk: int) -> JsonResponse:
+        pass
+
+    def delete(self, request: HttpRequest, pk: int) -> JsonResponse:
+        pass
 
 
 class ContactView(View):
-    pass
+    def get(self, reqeust: HttpRequest, pk: int) -> JsonResponse:
+        try:
+            customer = Customer.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return JsonResponse({'error': 'customer does not exist.'})
 
+        try:
+            contact = Contact.objects.get(customer=customer)
+        except (ObjectDoesNotExist, contact.DoesNotExist):
+            return JsonResponse({'error': 'contact does not exist.'})
+        
 
-class ContactDetailsView(View):
-    pass
+        result = model_to_dict(contact)
+        return JsonResponse(result)
 
+    def post(self, reqeust: HttpRequest, pk: int) -> JsonResponse:
+        try:
+            customer = Customer.objects.get(id=pk)
+        except ObjectDoesNotExist:
+            return JsonResponse({'error': 'customer does not exist.'})
+
+        data = json.loads(reqeust.body.decode())
+
+        contact = Contact.objects.create(
+            customer=customer, 
+            email=data.get('email'),
+            address=data.get('address'),
+            phone=data.get('phone')
+        )
+
+        result = model_to_dict(contact)
+        return JsonResponse(result, status=201)
+
+    def put(self, reqeust: HttpRequest) -> JsonResponse:
+        pass
+
+    def delete(self, reqeust: HttpRequest) -> JsonResponse:
+        pass 
